dbb8ba618f6b58149da41af36717b831c485e3a1
nickforsman <niclas.forsman@outlook.com>
2015-01-15 15:29:08 +0200

    new angular service

--- public/js/app.js
+++ public/js/app.js
@@ -1,10 +1,18 @@
 
 var app = angular.module('app', ['app.directives.confirmPassword']);
 
-// Controller gets json data from /request-users route
+/**
+*	This Controller will be finished once someone implements UI-Bootstrap
+*		
+*	UI-Bootstrap can be found @http://angular-ui.github.io/bootstrap/
+*/
+
+
+// Controller gets json data from /request/api/users/json
 app.controller("PostsCtrl", function($scope, $http) {
-	$http.get('/request-users').then(function(res) {
-		$scope.posts = res.data;
-		console.log(res.data);
+
+	$http.get('/request/api/users/json').then(function(res) {
+		// Test
+		$scope.posts = res.data; 
 	});
 });
